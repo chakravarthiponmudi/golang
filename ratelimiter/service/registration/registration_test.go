4eb956e59475ed60ae26e7c17d204fe4c968d7f6
chakravarthi ponmudi <chakravarthiponmudi@gmail.com>
2019-07-17 08:33:07 +0530

    working on testing

--- ratelimiter/service/registration/registration_test.go
+++ ratelimiter/service/registration/registration_test.go
@@ -6,35 +6,48 @@ import (
 )
 
 type mockContract struct {
+	testdata                        Contract
+	addContractResult               bool
+	deleteContractResult            bool
+	updateContractResult            bool
+	addAPIResult                    bool
+	getContractByNameAndGroupResult struct {
+		c ContractCRUD
+		e error
+	}
 }
 
 func (m *mockContract) addContract() bool {
-	return true
+	return m.addContractResult
 }
 
 func (m *mockContract) deleteContract() bool {
-	return true
+	return m.deleteContractResult
 }
 
 func (m *mockContract) updateContract() bool {
-	return true
+	return m.updateContractResult
 }
 
 func (m *mockContract) addAPI(apipath string) bool {
-	return true
+	return m.addAPIResult
 }
 
 func (m *mockContract) getContractByNameAndGroup(user string, group APIGroup) (ContractCRUD, error) {
 	fmt.Println("calling mockedgetContractByNameAndGroup by", m)
-	return &mockContract{}, nil
+	return m.getContractByNameAndGroupResult.c, m.getContractByNameAndGroupResult.e
 }
 
 func (m *mockContract) getObject() *Contract {
-	return new(Contract)
+	return &m.testdata
 }
 
 func TestRegisterContract(t *testing.T) {
 	var testContract = new(mockContract)
+	testContract.testdata = Contract{}
+	testContract.addContractResult = true
+	testContract.getContractByNameAndGroupResult.e = nil
+	testContract.getContractByNameAndGroupResult.c = &testContract.testdata
 	result := RegisterContract(testContract)
 	if !result {
 		t.Error(result)
